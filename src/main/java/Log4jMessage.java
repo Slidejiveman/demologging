9c0efa53bbb03dfba5579a6a897ab8d3df930b4a
Slidejiveman <Slidejiveman>
2018-05-27 15:41:24 -0500

    Initial commit

---
+++ src/main/java/Log4jMessage.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+public class Log4jMessage {
+
+    private static org.apache.log4j.Logger log = Logger.getLogger(Log4jMessage.class);
+
+    public static void main(String[] args) {
+        log.trace("Trace Message!");
+        log.debug("Debug Message!");
+        log.info("Info Message!");
+        log.warn("Warn Message!");
+        log.error("Error Message!");
+        log.fatal("Fatal Message!");
+    }
+}
